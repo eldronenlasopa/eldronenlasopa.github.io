5ee06d6e6a11502c642fa784c6224a7291e4563c
Jayro Guerreros <jayroguerreros@gmail.com>
2026-07-14 02:02:07 -0500

    Deploy build for GitHub Pages

--- src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-
-export const routes: Routes = [{ path: '', component: Home }];
--- src/app/pages/home/sections/pricing-section.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { PricingCard } from '../../../ui/marketing/pricing-card/pricing-card';
-
-@Component({
-  selector: 'app-pricing-section',
-  imports: [PricingCard],
-  templateUrl: './pricing-section.html',
-})
-export class PricingSection {}
